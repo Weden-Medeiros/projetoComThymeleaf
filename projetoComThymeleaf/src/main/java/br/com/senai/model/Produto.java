b6d7f06af05d972899105909bf378007c6e659c5
Weden-Medeiros <Wedenbsb@gmail.com>
2023-08-14 19:52:01 -0300

    Proejto projeto com Thymeleaf em java

---
+++ projetoComThymeleaf/src/main/java/br/com/senai/model/Produto.java
@@ -0,0 +1,66 @@
+package br.com.senai.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull (message = "Campo obrigatório")
+	@Size (min =2, max= 254, message="Nome deve conter entre 2 e 254 caracteres")
+	private String nome;
+	private int quantidade;
+	private String  preco;
+	private String descricaoProduto;
+	private String urlImagem;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public String getPreco() {
+		return preco;
+	}
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	
+	
+	
+		
+
+}
